a24877537aa81c37623d9e3b6be70c625e51e220
Otso Toivonen <otso.toivonen@tuni.fi>
2025-11-25 08:30:39 +0200

    Better test description.

--- src/defaultTo.test.js
+++ src/defaultTo.test.js
@@ -2,47 +2,43 @@ import defaultTo from './defaultTo.js';
 
 describe('defaultTo', () => {
 
-  test('defaultTo-value-1' ,() => {
+  test('returns value 1' ,() => {
     expect(defaultTo(1, 100)).toBe(1);
   });
 
-  test('defaultTo-value-2' ,() => {
+  test('returns value false' ,() => {
     expect(defaultTo(false, 100)).toBe(false);
   });
 
-  test('defaultTo-value-3' ,() => {
+  test('returns value true' ,() => {
     expect(defaultTo(true, 100)).toBe(true);
   });
 
-  test('defaultTo-value-4' ,() => {
+  test('returns value 0' ,() => {
     expect(defaultTo(0, 100)).toBe(0);
   });
 
-  test('defaultTo-value-5' ,() => {
+  test('returns value empty string' ,() => {
     expect(defaultTo("", 100)).toBe("");
   });
 
-  test('defaultTo-default-1' ,() => {
+  test('defaults to number from null' ,() => {
     expect(defaultTo(null, 100)).toBe(100);
   });
 
-  test('defaultTo-default-2' ,() => {
+  test('defaults to string from null' ,() => {
     expect(defaultTo(null, "str")).toBe("str");
   });
 
-  test('defaultTo-default-3' ,() => {
-    expect(defaultTo(null, 401)).toBe(401);
-  });
-
-  test('defaultTo-null-1' ,() => {
+  test('both inputs null returns null' ,() => {
     expect(defaultTo(null, null)).toBe(null);
   });
 
-  test('defaultTo-NaN-1' ,() => {
+  test('defaults from NaN' ,() => {
     expect(defaultTo(NaN, 100)).toBe(100);
   });
 
-  test('defaultTo-undefined-1' ,() => {
+  test('defaults from undefined' ,() => {
     expect(defaultTo(undefined, 100)).toBe(100);
   });
 
